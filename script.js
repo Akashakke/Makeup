3fd4e8d81fb119ce3e82b06c7c456f2a5ff0d23d
Akash praveen kumar R <98536933+Akashakke@users.noreply.github.com>
2022-04-12 14:24:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+const getData = async () => {
+  let response = await fetch(
+    "https://makeup-api.herokuapp.com/api/v1/products.json"
+  );
+
+  const data = await response.json();
+  console.log(data);
+  for (let i = 0; data.length; i++) {
+  if(data[i].image_link && data[i].brand && data[i].name && data[i].price) {
+    var ele = document.getElementById("card");
+    ele.innerHTML += `
+           <div class="card-type" style="width: 18rem;">
+<img src="${data[i].image_link}" class="card-img-top" alt="">
+<div class="card-body">
+  <p class="card-text">Brand: ${data[i].brand}
+  Name: ${data[i].name}
+  Price: ${data[i].price}</p>
+</div>
+</div>`;
+}
+  }
+};
+getData();
+
